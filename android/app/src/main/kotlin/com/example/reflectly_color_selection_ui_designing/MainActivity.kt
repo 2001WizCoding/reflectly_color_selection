63612b9e558258e9a51d42ad323665fa8401adcd
Yamin Nather <2001wiz.coding@gmail.com>
2021-08-25 16:59:22 +0530

    Full project commit.
    Full project commit.

---
+++ android/app/src/main/kotlin/com/example/reflectly_color_selection_ui_designing/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.reflectly_color_selection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:reflectly_color_selection/app/app_widget.dart';
+
+void main() {
+  runApp(const WApp());
+}
